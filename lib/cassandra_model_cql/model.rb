6f1aca96bf0abfd944750073ab55c4396067ce7b
Alexey Kruk <alexey.kruk@rightscale.com>
2012-07-31 11:38:29 +0300

    Model is restored

--- lib/cassandra_model_cql/model.rb
+++ lib/cassandra_model_cql/model.rb
@@ -19,7 +19,7 @@ module CassandraModelCql
   # t = Timeline.new(:user_id=>'mickey', :tweet_id=>1715, :author=> 'mouse', :body=>"'Hello!'")
   # t.save
   #
-  # timelines = Timeline.find('mickey')
+  # timelines = Timeline.all('mickey')
   # t = timelines.first
   # puts t.body
   # => 'Hello!'
@@ -32,95 +32,6 @@ module CassandraModelCql
   #
   # #dropping Column Family
   # Timeline.drop_table
-  class Model
-
-    attr_accessor :last_error, :last_error_command
-
-    def initialize(*args)
-      @attributes = {}
-      @options = {:primary_keys => [], :key_space => nil, :dirty => false}
-    end
-
-    def attribute(name, type)
-      self.last_error = nil
-      self.last_error = 'Attribute name must be a Symbol' if name.class != Symbol
-      self.last_error = 'Attribute type must be a String' if type.class != String
-      self.last_error = 'Attribute type must be a valid Data Type' if type.size == 0
-
-      @attributes[name]={:value => nil, :type => type, :dirty => true }
-
-      self.class.send(:define_method, name) do
-        @attributes[name][:value]
-      end
-      self.class.send(:define_method, :"#{name}=") do |value|
-        @attributes[name][:value] = value
-        @attributes[name][:dirty] = true
-        @options[:dirty] = true
-      end
-
-      self.last_error_command = 'attribute' if self.last_error != nil
-    end
-
-    def primary_key(*columns)
-      columns.each do |key|
-        if @attributes[key]
-          @options[:primary_keys].push(key)
-        else
-          self.last_error = 'Unknown attribute'
-          self.last_error_command = 'primary_key'
-          @options[:primary_keys]=[]
-        end
-      end
-    end
-
-    def set_keyspace(keyspace)
-      @options[:keyspace] = keyspace
-    end
-
-    def keyspace
-      @options[:keyspace]
-    end
-
-    def create_table
-      self.last_error = nil
-    end
-
-    def drop_table
-      self.last_error = nil
-    end
-
-    def update(*columns)
-    # columns - arrays of pairs: key => value
-      self.last_error = nil
-    end
-
-    def dirty?
-      self.last_error = nil
-      @dirty
-    end
-
-    def save
-      self.last_error = nil
-    end
-
-    def delete(clauses = {})
-      self.last_error = nil
-    end
-
-    def find(*args, &blk)
-    # args[0] : primary key
-    # args[1...]: clauses
-      self.last_error = nil
-    end
-
-    def all(*args, &blk)
-    # args[0] : primary key
-    # args[1...]: clauses
-      self.last_error = nil
-    end
-
-    #end
-
+  class Model < Table
   end
-
 end
