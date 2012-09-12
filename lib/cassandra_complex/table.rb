502caa1606fa8833e1b17cb60f6514be1d497eb8
Sergey Enin <sergeyenin@gmail.com>
2012-09-12 15:23:29 +0300

    Added possibility to set table name.

--- lib/cassandra_complex/table.rb
+++ lib/cassandra_complex/table.rb
@@ -21,6 +21,8 @@ module CassandraComplex
       attr_accessor :keyspace
       attr_accessor :configuration
 
+      attr_reader   :tbl_name
+
       def set_keyspace(kyspc)
         self.keyspace = kyspc
       end
@@ -33,8 +35,12 @@ module CassandraComplex
         connection.with_keyspace(kyspc, &blck)
       end
 
+      def set_table_name(tbl)
+        self.tbl_name = tbl
+      end
+
       def table_name
-        self.name.downcase
+        self.tbl_name || self.name.downcase
       end
 
       def id
